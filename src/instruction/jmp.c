3fd204aaa46ebc1390284c8466662e64063169ad
Ryan Webber <wbbr.ryan@gmail.com>
2018-07-30 23:38:24 +0000

    Fixed printing of instructions

--- src/instruction/jmp.c
+++ src/instruction/jmp.c
@@ -6,7 +6,7 @@
  */
 uint8_t inst_jmp(BloomCPU* cpu) {
 	uint16_t *addr = (uint16_t*) (cpu->memory + cpu->pc + 1);
-	printf("0x%04X JMP 0x%04X\n", cpu->pc, *addr);
+	printf("0x%04X JMP 0x%04X\n", cpu->pc * 8, *addr);
 	cpu->pc = *addr;
 	return 0;
 }
--- src/instruction/nop.c
+++ src/instruction/nop.c
@@ -5,7 +5,7 @@
  * Just increase the program counter by 1
  */
 uint8_t inst_nop(BloomCPU* cpu) {
-	printf("0x%04X NOP\n", cpu->pc);
+	printf("0x%04X NOP\n", cpu->pc * 8);
 	cpu->pc += 1;
 	return 0;
 }
