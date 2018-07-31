882f9c0c77262c5dad6357d7cec091b0f2956e35
Ryan Webber <wbbr.ryan@gmail.com>
2018-07-31 23:56:43 +0000

    Added more instructions

--- src/cpu.c
+++ src/cpu.c
@@ -2,9 +2,12 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 uint8_t _step(BloomCPU*);
 void _unsupported_opcode(BloomCPU*, uint8_t);
+void _debug_instruction(BloomCPU*, const char*, uint8_t);
+void _push(BloomCPU*, uint8_t*, uint16_t);
 
 BloomCPU* cpu_create() {
 	
@@ -57,13 +60,47 @@ uint8_t cpu_step(BloomCPU* cpu) {
 	uint8_t *opcode = &cpu->memory[cpu->pc];
 	uint8_t result;
 	switch(*opcode) {
-		case 0x00: 
+		case 0x00: // nop 
+			_debug_instruction(cpu, "NOP", 0);
 			cpu->pc++;
 			break;
-		case 0xC3:
+		case 0x06: // MVI D |8|
+			_debug_instruction(cpu, "MVI D |8|", 1);
+			cpu->h = opcode[1];
+			cpu->pc++;
+			break;
+		case 0x11: // lxi d
+			_debug_instruction(cpu, "LXI D", 2);
+			cpu->d = opcode[1];
+			cpu->e = opcode[2];
+			cpu->pc++;
+			break;
+		case 0x1B: // dcx d
+			_debug_instruction(cpu, "DCX D", 0);
+			{
+				uint16_t num = (cpu->e << 8 | cpu->d);
+				num--;
+				cpu->d = num & 0x00FF;
+				cpu->e = (num & 0xFF00) >> 8;
+				cpu->pc++;
+			}
+			break;
+		case 0x24: // inr h
+			_debug_instruction(cpu, "INR H", 0);
+			cpu->h++;
+			cpu->pc++;
+			break;
+		case 0xC3: // jmp
+			_debug_instruction(cpu, "JMP", 2);
 			cpu->pc = (opcode[2] << 8) | opcode[1];
 			break;
-		case 0x31:
+		case 0xcd: // call
+			_debug_instruction(cpu, "CALL", 2);
+			_push(cpu, cpu->memory + cpu->pc + 1, 2);
+			cpu->pc = (opcode[2] << 8 | opcode[1]);
+			break;
+		case 0x31: //lxi sp
+			_debug_instruction(cpu, "LXI SP", 2);
 			cpu->sp = (opcode[2] << 8) | opcode[1];
 			cpu->pc++;
 			break;
@@ -82,5 +119,18 @@ void cpu_destroy(BloomCPU* cpu) {
 
 void _unsupported_opcode(BloomCPU* cpu, uint8_t opcode) {
 	printf("\nUnimplemented instruction:\n0x%04X 0x%02X\n", cpu->pc, opcode);
+}
+
+void _debug_instruction(BloomCPU* cpu, const char* inst, uint8_t argc) {
+	printf("0x%04X %-16s ; ", cpu->pc, inst);
+	for (uint8_t i = 0; i < argc; i++) {
+		printf("0x%02X ", cpu->memory[cpu->pc + i + 1]);
+	}
+	printf("\n");
+}
+
+void _push(BloomCPU* cpu, uint8_t *addr, uint16_t len) {
+	memcpy(cpu->memory + cpu->sp, addr, len);
+	cpu->sp += len;
 }
 
--- test/check_bloom.c
+++ test/check_bloom.c
@@ -5,12 +5,6 @@
 
 #include "../src/cpu.h"
 
-void setup(void){
-}
-
-void teardown(void){
-}
-
 START_TEST(test_bloom_init)
 {
 	/* TODO, initialize bloom test */
@@ -68,6 +62,106 @@ START_TEST(test_bloom_inst_lxi_sp)
 }
 END_TEST
 
+START_TEST(test_bloom_inst_lxi_d)
+{
+	BloomCPU *cpu = cpu_create();
+	uint8_t rom[3] = {
+		0x11, 0xFF, 0xFE
+	};
+	
+	cpu_initialize_rom(cpu, rom, 16, 0);
+	
+	uint8_t result = cpu_step(cpu);
+	ck_assert_uint_eq(result, 0);
+	ck_assert_uint_eq(cpu->d, 0xFF);
+	ck_assert_uint_eq(cpu->e, 0xFE);
+	
+	cpu_destroy(cpu);
+}
+END_TEST
+
+START_TEST(test_bloom_inst_inr_h)
+{
+	BloomCPU *cpu = cpu_create();
+	uint8_t rom[4] = {
+		0x24
+	};
+	
+	cpu_initialize_rom(cpu, rom, 24, 0);
+	
+	uint8_t result = cpu_step(cpu);
+	ck_assert_uint_eq(result, 0);
+	ck_assert_uint_eq(cpu->h, 0x1);
+	
+	cpu_destroy(cpu);
+}
+END_TEST
+
+START_TEST(test_bloom_inst_mvi_h)
+{
+	BloomCPU *cpu = cpu_create();
+	uint8_t rom[4] = {
+		0x06, 0x55
+	};
+	
+	cpu_initialize_rom(cpu, rom, 24, 0);
+	
+	uint8_t result = cpu_step(cpu);
+	ck_assert_uint_eq(result, 0);
+	ck_assert_uint_eq(cpu->h, 0x55);
+	
+	cpu_destroy(cpu);
+}
+END_TEST
+
+START_TEST(test_bloom_inst_call)
+{
+	BloomCPU *cpu = cpu_create();
+	uint8_t *rom = malloc(sizeof(uint8_t));
+	rom[0] = 0xcd;
+	rom[1] = 0xfe;
+	rom[2] = 0x55;
+	rom[3] = 0x00;
+	rom[4] = 0x00;
+	
+	cpu_initialize_rom(cpu, rom, 60, 0);
+	cpu->sp = 3;
+	
+	uint8_t result = cpu_step(cpu);
+	ck_assert_uint_eq(result, 0);
+	ck_assert_uint_eq(cpu->sp, 0x5);
+	ck_assert_uint_eq(cpu->memory[3], 0xFE);
+	ck_assert_uint_eq(cpu->memory[4], 0x55);
+	
+	cpu_destroy(cpu);
+	free(rom);
+}
+END_TEST
+
+START_TEST(test_bloom_inst_dcx_d)
+{
+	BloomCPU *cpu = cpu_create();
+	uint8_t rom[1] = {
+		0x1B
+	};
+	
+	cpu_initialize_rom(cpu, rom, 4, 0);
+	uint8_t reg[2] = {
+		0x32, 0x10
+	};
+
+	cpu->d = reg[0];
+	cpu->e = reg[1];
+
+	uint8_t result = cpu_step(cpu);
+	ck_assert_uint_eq(result, 0);
+	ck_assert_uint_eq(cpu->d, reg[0] - 1);
+	ck_assert_uint_eq(cpu->e, reg[1]);
+	
+	cpu_destroy(cpu);
+}
+END_TEST
+
 Suite* bloom_suite(void){
     Suite *s;
     TCase *tc_core;
@@ -77,14 +171,18 @@ Suite* bloom_suite(void){
 
     /* Core test case */
     tc_core = tcase_create("Core");
-    tcase_add_checked_fixture(tc_core, setup, teardown);
     tcase_add_test(tc_core, test_bloom_init);
     suite_add_tcase(s, tc_core);
 
     /* Instructions test case */
     tc_instr = tcase_create("Instructions");
+    tcase_add_test(tc_instr, test_bloom_inst_call);
+    tcase_add_test(tc_instr, test_bloom_inst_dcx_d);
+    tcase_add_test(tc_instr, test_bloom_inst_inr_h);
     tcase_add_test(tc_instr, test_bloom_inst_jmp);
+    tcase_add_test(tc_instr, test_bloom_inst_lxi_d);
     tcase_add_test(tc_instr, test_bloom_inst_lxi_sp);
+    tcase_add_test(tc_instr, test_bloom_inst_mvi_h);
     tcase_add_test(tc_instr, test_bloom_inst_nop);
 	suite_add_tcase(s, tc_instr);
 
