a8914d0a7c0b513f6a8ff55865f0325d1d72f566
Ryan Webber <wbbr.ryan@gmail.com>
2018-08-11 14:29:54 +0000

    Improved testing prints

--- test/assertions.h
+++ test/assertions.h
@@ -2,7 +2,7 @@
 #define ASSERT_H
 
 void _assert_uint_eq(unsigned int a, unsigned int b, const char *file, int line) {
-	printf("\nAssertion failed: Expected [%u] but got [%u] (%s:%i)\n\n", a, b, file, line);
+	fprintf(stderr, "\nAssertion failed: Expected [%u] but got [%u] (%s:%i)\n\n", a, b, file, line);
 }
 #define assert_uint_eq(a,b) if(a != b){ _assert_uint_eq(a,b,__FILE__, __LINE__); return 1; }
 
